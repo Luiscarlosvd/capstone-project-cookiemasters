aa0a82f327ff27112ec95980bcda4767fc56d1d3
Luiscarlosvd <lc26cr7@gmail.com>
2023-04-16 12:17:52 -0400

    Fixed linters errors

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
 /* Featured Guests */
 
 const guests = [
-    {
-        image: "img/jordi-roca-pic (2).jpeg",
-        Name: "Jordi Roca",
-        subtitle: "Pastry chef at El Celler de Can Roca restaurant",
-        description: `Spanish Chef, In 2014 he won the inaugural award for "Best Pastry Chef in the World" from Restaurant magazine.`,
-    },
-    {
-        image: "img/Kirsten-tibballs (4).jpg",
-        Name: "Jordi Roca",
-        subtitle: "Pastry chef at El Celler de Can Roca restaurant",
-        description: `Spanish Chef, In 2014 he won the inaugural award for "Best Pastry Chef in the World" from Restaurant magazine.`,
-    },
-    {
-        image: "img/jordi-roca-pic (2).jpeg",
-        Name: "Jordi Roca",
-        subtitle: "Pastry chef at El Celler de Can Roca restaurant",
-        description: `Spanish Chef, In 2014 he won the inaugural award for "Best Pastry Chef in the World" from Restaurant magazine.`,
-    },
-    {
-        image: "img/jordi-roca-pic (2).jpeg",
-        Name: "Jordi Roca",
-        subtitle: "Pastry chef at El Celler de Can Roca restaurant",
-        description: `Spanish Chef, In 2014 he won the inaugural award for "Best Pastry Chef in the World" from Restaurant magazine.`,
-    },
-    {
-        image: "img/jordi-roca-pic (2).jpeg",
-        Name: "Jordi Roca",
-        subtitle: "Pastry chef at El Celler de Can Roca restaurant",
-        description: `Spanish Chef, In 2014 he won the inaugural award for "Best Pastry Chef in the World" from Restaurant magazine.`,
-    },
-    {
-        image: "img/jordi-roca-pic (2).jpeg",
-        Name: "Jordi Roca",
-        subtitle: "Pastry chef at El Celler de Can Roca restaurant",
-        description: `Spanish Chef, In 2014 he won the inaugural award for "Best Pastry Chef in the World" from Restaurant magazine.`,
-    },
+  {
+    image: 'img/jordi-roca-pic (2).jpeg',
+    Name: 'Jordi Roca',
+    subtitle: 'Pastry chef at El Celler de Can Roca restaurant',
+    description: 'Spanish Chef, In 2014 he won the inaugural award for "Best Pastry Chef in the World" from Restaurant magazine.',
+  },
+  {
+    image: 'img/Kirsten-tibballs (4).jpg',
+    Name: 'Jordi Roca',
+    subtitle: 'Pastry chef at El Celler de Can Roca restaurant',
+    description: 'Spanish Chef, In 2014 he won the inaugural award for "Best Pastry Chef in the World" from Restaurant magazine.',
+  },
+  {
+    image: 'img/jordi-roca-pic (2).jpeg',
+    Name: 'Jordi Roca',
+    subtitle: 'Pastry chef at El Celler de Can Roca restaurant',
+    description: 'Spanish Chef, In 2014 he won the inaugural award for "Best Pastry Chef in the World" from Restaurant magazine.',
+  },
+  {
+    image: 'img/jordi-roca-pic (2).jpeg',
+    Name: 'Jordi Roca',
+    subtitle: 'Pastry chef at El Celler de Can Roca restaurant',
+    description: 'Spanish Chef, In 2014 he won the inaugural award for "Best Pastry Chef in the World" from Restaurant magazine.',
+  },
+  {
+    image: 'img/jordi-roca-pic (2).jpeg',
+    Name: 'Jordi Roca',
+    subtitle: 'Pastry chef at El Celler de Can Roca restaurant',
+    description: 'Spanish Chef, In 2014 he won the inaugural award for "Best Pastry Chef in the World" from Restaurant magazine.',
+  },
+  {
+    image: 'img/jordi-roca-pic (2).jpeg',
+    Name: 'Jordi Roca',
+    subtitle: 'Pastry chef at El Celler de Can Roca restaurant',
+    description: 'Spanish Chef, In 2014 he won the inaugural award for "Best Pastry Chef in the World" from Restaurant magazine.',
+  },
 ];
 
-const guestContainer = document.getElementById("guest-container");
+const guestContainer = document.getElementById('guest-container');
 
 guests.forEach((x) => {
-    guestContainer.innerHTML += `
+  guestContainer.innerHTML += `
         <li class="individual-guest">
             <img src="${x.image}" alt="">
             <div class="description-guest">
